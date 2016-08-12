2227ae0b094dcaa6b4312a3d394766f2630dd107
Philip Dexter <philip.dexter@gmail.com>
2016-08-12 22:58:45 +0100

    Add math function

--- spandex/elastic.ml
+++ spandex/elastic.ml
@@ -1,3 +1,5 @@
+let spf = Printf.sprintf
+
 let print_all =
   List.iter print_endline
 
@@ -5,3 +7,5 @@ let enumerate ls =
   ["\\begin{enumerate}"] @
   List.map (fun s -> "\\item " ^ s ^ "\n") ls @
   ["\\end{enumerate}"]
+
+let math = spf "\( %s \)"
--- spandex/elastic.mli
+++ spandex/elastic.mli
@@ -1,3 +1,5 @@
 val print_all : string list -> unit
 
 val enumerate : string list -> string list
+
+val math : string -> string
