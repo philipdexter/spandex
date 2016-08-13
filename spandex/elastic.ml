d4d8aa0705f585dafd4440ff0f03f53e73cf95fd
Philip Dexter <philip.dexter@gmail.com>
2016-08-13 19:08:19 +0100

    Change Elastic API

--- spandex/elastic.ml
+++ spandex/elastic.ml
@@ -1,11 +1,13 @@
 let spf = Printf.sprintf
 
-let print_all =
-  List.iter print_endline
-
 let enumerate ls =
   ["\\begin{enumerate}"] @
   List.map (fun s -> "\\item " ^ s ^ "\n") ls @
   ["\\end{enumerate}"]
 
-let math = spf "\( %s \)"
+let math = spf "\\( %s \\)"
+
+let out = print_endline
+let outf f s = Printf.printf "%s\n" (Printf.sprintf f s)
+let out_all =
+  List.iter print_endline
--- spandex/elastic.mli
+++ spandex/elastic.mli
@@ -1,5 +1,10 @@
-val print_all : string list -> unit
-
 val enumerate : string list -> string list
 
 val math : string -> string
+
+(** print string to stdout followed by a newline *)
+val out : string -> unit
+(** print formatted string to stdout followed by a newline *)
+val outf : ('a -> string, unit, string) format -> 'a -> unit
+(** print list of strings, each followed by a newline, to stdout *)
+val out_all : string list -> unit
